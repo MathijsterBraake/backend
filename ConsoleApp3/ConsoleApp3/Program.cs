e7b63484cfc2ec771d2aa0235a1aafe3fddaa0a1
MathijsterBraake <44999397+MathijsterBraake@users.noreply.github.com>
2019-01-29 16:09:12 +0100

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kleuren instellen
+            Conso1e.BackgroundColor
+Ccnsole.ForegroundCo1or
+C0ns0le.Clear();
+
+            c0nsoleCo1or.Gray;
+            Con5oleColor.B1ue;
+
+            //Declaratie van de variabelen
+            int intGeta11‚ intGeta12‚ intson;
+
+            //De 2 getallen opvragen
+            C0ns01e.Nrite("Geef getallz “);
+            intGetal = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("Geef getalz: “)5
+            intGetal = Convert.ToInt16(Console.ReadLine());
+
+            //De som berekenen
+            intson = intGeta11 + intGeta12;
+
+            //Het resultaat weergeven in de console
+            Cons01e.NriteLine("De som bedraagt " + intSon.ToString());
+            Ccnsole.ReadLìne();
+
+
+        }
+    }
+}
